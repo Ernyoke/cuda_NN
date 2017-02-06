3179cd7c487b1d17830e8b19cec2dcaa38e5fec5
Ervin_MAIN <ervin509@yahoo.com>
2017-02-06 19:55:28 +0200

    Final version.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cuda_NN
+Building the project under Windows:
+
+1. Import into VisualStudio 15 or newer
+2. Build the solution
+3. Run 
--- cuda_NN/kernel.cu
+++ cuda_NN/kernel.cu
@@ -13,55 +13,221 @@
 #include "Net_GPU.cuh"
 #include "Utilities.h"
 
-int main()
+//create nets for CPU testing
+Net TestNet1_CPU()
 {
-	/*
-	double a[30] = { 0.1000, 0.2000, 0.3000, 0.4000, 0.5000, 0.6000, 0.7000, 0.8000, 0.9000, 1.0000, 1.1000, 1.2000, 1.3000, 1.4000, 1.5000, 1.6000, 1.7000, 1.8000, 1.9000, 2.0000, 2.1000, 2.2000, 2.3000, 2.4000, 2.5000, 2.6000, 2.7000, 2.8000, 2.9000, 3.0000 };
-	double res[30] = { 0.0998, 0.1987, 0.2955, 0.3894, 0.4794, 0.5646, 0.6442, 0.7174, 0.7833, 0.8415, 0.8912, 0.9320, 0.9636, 0.9854, 0.9975, 0.9996, 0.9917, 0.9738, 0.9463, 0.9093, 0.8632, 0.8085, 0.7457, 0.6755, 0.5985, 0.5155, 0.4274, 0.3350, 0.2392, 0.1411 };
-	*/
-
-	utils::mnist mn;
-	std::cout << "Reading mnist data..." << std::endl;
-	utils::ReadMnist(mn);
-	std::cout << "Creating net..." << std::endl;
-
-	std::shared_ptr<Layer_GPU> layer1(new Layer_GPU(784, 128));
+	Net net;
+	std::shared_ptr<Layer> layer1(new Layer(784, 128));
 	layer1->InitWeights();
-	std::shared_ptr<Layer_GPU> layer2(new Layer_GPU(128, 10));
+	std::shared_ptr<Layer> layer2(new Layer(128, 128));
 	layer2->InitWeights();
+	std::shared_ptr<Layer> outputlayer(new Layer(128, 10));
+	outputlayer->InitWeights();
 
-	Net_GPU net;
 	net.AddLayer(layer1);
 	net.AddLayer(layer2);
+	net.AddLayer(outputlayer);
+	return net;
+}
 
-	net.SetMomentum(0.5);
-	net.SetTrainRate(0.001);
+Net TestNet2_CPU()
+{
+	Net net;
+	std::shared_ptr<Layer> layer1(new Layer(784, 128));
+	layer1->InitWeights();
+	std::shared_ptr<Layer> layer2(new Layer(128, 128));
+	layer2->InitWeights();
+	std::shared_ptr<Layer> layer3(new Layer(128, 128));
+	layer3->InitWeights();
+	std::shared_ptr<Layer> outputlayer(new Layer(128, 10));
+	outputlayer->InitWeights();
 
-	net.InitInputs(mn.images, mn.labels, mn.samples, mn.height * mn.width, mn.classes);
+	net.AddLayer(layer1);
+	net.AddLayer(layer2);
+	net.AddLayer(layer3);
+	net.AddLayer(outputlayer);
+	return net;
+}
 
+Net TestNet3_CPU()
+{
+	Net net;
+	std::shared_ptr<Layer> layer1(new Layer(784, 1024));
+	layer1->InitWeights();
+	std::shared_ptr<Layer> layer2(new Layer(1024, 1024));
+	layer2->InitWeights();
+	std::shared_ptr<Layer> outputlayer(new Layer(1024, 10));
+	outputlayer->InitWeights();
+
+	net.AddLayer(layer1);
+	net.AddLayer(layer2);
+	net.AddLayer(outputlayer);
+	return net;
+}
+
+Net TestNet4_CPU()
+{
+	Net net;
+	std::shared_ptr<Layer> layer1(new Layer(784, 1024));
+	layer1->InitWeights();
+	std::shared_ptr<Layer> layer2(new Layer(1024, 1024));
+	layer2->InitWeights();
+	std::shared_ptr<Layer> layer3(new Layer(1024, 1024));
+	layer3->InitWeights();
+	std::shared_ptr<Layer> outputlayer(new Layer(1024, 10));
+	outputlayer->InitWeights();
+
+	net.AddLayer(layer1);
+	net.AddLayer(layer2);
+	net.AddLayer(layer3);
+	net.AddLayer(outputlayer);
+	return net;
+}
+
+//create nets for GPU testing
+Net_GPU TestNet1_GPU()
+{
+	Net_GPU net;
+	std::shared_ptr<Layer_GPU> layer1(new Layer_GPU(784, 128));
+	layer1->InitWeights();
+	std::shared_ptr<Layer_GPU> layer2(new Layer_GPU(128, 128));
+	layer2->InitWeights();
+	std::shared_ptr<Layer_GPU> outputlayer(new Layer_GPU(128, 10));
+	outputlayer->InitWeights();
+
+	net.AddLayer(layer1);
+	net.AddLayer(layer2);
+	net.AddLayer(outputlayer);
+	return net;
+}
+
+Net_GPU TestNet2_GPU()
+{
+	Net_GPU net;
+	std::shared_ptr<Layer_GPU> layer1(new Layer_GPU(784, 128));
+	layer1->InitWeights();
+	std::shared_ptr<Layer_GPU> layer2(new Layer_GPU(128, 128));
+	layer2->InitWeights();
+	std::shared_ptr<Layer_GPU> layer3(new Layer_GPU(128, 128));
+	layer3->InitWeights();
+	std::shared_ptr<Layer_GPU> outputlayer(new Layer_GPU(128, 10));
+	outputlayer->InitWeights();
+
+	net.AddLayer(layer1);
+	net.AddLayer(layer2);
+	net.AddLayer(layer3);
+	net.AddLayer(outputlayer);
+	return net;
+}
+
+Net_GPU TestNet3_GPU()
+{
+	Net_GPU net;
+	std::shared_ptr<Layer_GPU> layer1(new Layer_GPU(784, 1024));
+	layer1->InitWeights();
+	std::shared_ptr<Layer_GPU> layer2(new Layer_GPU(1024, 1024));
+	layer2->InitWeights();
+	std::shared_ptr<Layer_GPU> outputlayer(new Layer_GPU(1024, 10));
+	outputlayer->InitWeights();
+
+	net.AddLayer(layer1);
+	net.AddLayer(layer2);
+	net.AddLayer(outputlayer);
+	return net;
+}
+
+Net_GPU TestNet4_GPU()
+{
+	Net_GPU net;
+	std::shared_ptr<Layer_GPU> layer1(new Layer_GPU(784, 1024));
+	layer1->InitWeights();
+	std::shared_ptr<Layer_GPU> layer2(new Layer_GPU(1024, 1024));
+	layer2->InitWeights();
+	std::shared_ptr<Layer_GPU> layer3(new Layer_GPU(1024, 1024));
+	layer3->InitWeights();
+	std::shared_ptr<Layer_GPU> outputlayer(new Layer_GPU(1024, 10));
+	outputlayer->InitWeights();
+
+	net.AddLayer(layer1);
+	net.AddLayer(layer2);
+	net.AddLayer(layer3);
+	net.AddLayer(outputlayer);
+	return net;
+}
+
+
+void TrainCPU(Net && net, utils::mnist& mn)
+{
 	std::cout << "Starting training..." << std::endl;
+	long long totalDuration = 0;
 	for (auto x = 0; x < 100; ++x) {
 		std::cout << "Epoch: " << x << std::endl;
-		for (auto i = 0; i < 10; ++i)
+		for (auto i = 0; i < 100; ++i)
 		{
 			auto globalError = 0.0;
 			std::cout << "Sample " << i << std::endl;
+			auto start = std::chrono::steady_clock::now();
 			for (auto j = 0; j < 10; ++j)
 			{
-				auto start = std::chrono::steady_clock::now();
-				net.Train_GPU(i);
-				auto duration = std::chrono::steady_clock::now() - start;
-				//std::cout << "Clock: " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << std::endl;
+				net.Train(mn.images[i], mn.labels[i]);
 
 				globalError = net.GetError();
-				std::cout << globalError << std::endl;
-				//std::cout << error << std::endl;
 			}
-			//globalError /= 10;
-			std::cout << globalError << std::endl;
+			auto duration = std::chrono::steady_clock::now() - start;
+			totalDuration = totalDuration + duration.count();
 		}
+		std::cout << "Average iteration duration per epoch: " << totalDuration / 100 << std::endl;
 	}
+}
 
+void TrainGPU(Net_GPU && net,  utils::mnist& mn)
+{
+	net.InitInputs(mn.images, mn.labels, mn.samples, mn.height * mn.width, mn.classes);
+
+	std::cout << "Starting training..." << std::endl;
+	long long totalDuration = 0;
+	for (auto x = 0; x < 100; ++x) {
+		std::cout << "Epoch: " << x << std::endl;
+		for (auto i = 0; i < 100; ++i)
+		{
+			auto globalError = 0.0;
+			std::cout << "Sample " << i << std::endl;
+			auto start = std::chrono::steady_clock::now();
+			for (auto j = 0; j < 10; ++j)
+			{
+				net.Train_GPU(i);
+
+				globalError = net.GetError();
+			}
+			auto duration = std::chrono::steady_clock::now() - start;
+			totalDuration = totalDuration + duration.count();
+		}
+		std::cout << "Average iteration duration per epoch: " << totalDuration / 100 << std::endl;
+	}
+}
+
+int main()
+{
+	utils::mnist mn;
+	std::cout << "Reading mnist data..." << std::endl;
+	utils::ReadMnist(mn);
+
+	std::cout << "Runnign test 1. for CPU" << std::endl;
+	TrainCPU(TestNet1_CPU(), mn);
+	std::cout << "Runnign test 1. for GPU" << std::endl;
+	TrainGPU(TestNet1_GPU(), mn);
+	std::cout << "Runnign test 2. for CPU" << std::endl;
+	TrainCPU(TestNet2_CPU(), mn);
+	std::cout << "Runnign test 2. for GPU" << std::endl;
+	TrainGPU(TestNet2_GPU(), mn);
+	std::cout << "Runnign test 3. for CPU" << std::endl;
+	TrainCPU(TestNet3_CPU(), mn);
+	std::cout << "Runnign test 3. for GPU" << std::endl;
+	TrainGPU(TestNet3_GPU(), mn);
+	std::cout << "Runnign test 4. for CPU" << std::endl;
+	TrainCPU(TestNet4_CPU(), mn);
+	std::cout << "Runnign test 4. for GPU" << std::endl;
+	TrainGPU(TestNet4_GPU(), mn);
 
 	return 0;
 }
